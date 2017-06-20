9bc5910b2b0365b8529259e778505ac9ac87fc08
vThaian <thaian@outlook.com>
2017-06-20 11:51:13 +0200

    Added JWT to auth classes in settings

--- hyperion/settings.py
+++ hyperion/settings.py
@@ -87,7 +87,12 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+            'rest_framework.authentication.SessionAuthentication',
+            'rest_framework.authentication.BasicAuthentication',
+        ),
 }
 
 
